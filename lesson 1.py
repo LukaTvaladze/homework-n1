f326c0d48ffb3fe68b079d91dc6c9dd30e508aaf
LukaTvaladze <113187213+LukaTvaladze@users.noreply.github.com>
2022-09-24 19:14:36 +0400

    Add files via upload

---
+++ lesson 1.py
@@ -0,0 +1,31 @@
+# name = "nika"
+
+
+
+
+# full_name = "nika keshelava"
+# print(len(full_name))
+
+# print(full_name[0])
+
+
+# full_name = "nika keshelava"
+
+# #ყველა სტრინგი შეგვიძლია მივიჩნიოთ სიად
+# # print(full_name[7])
+# #position = index სინონიმებია
+
+# # print("s" not in full_name)
+
+# # full_name = "nika keshelava"
+# # print(full_name[2:9])
+# # print(full_name[2:9:2])
+
+# full_name = "luka tvaladze"
+
+# print (full_name[2:5])
+# print (full_name[3:6])
+
+full_name = "luka tvaladze"
+
+print (full_name[-1:-6:-1])
